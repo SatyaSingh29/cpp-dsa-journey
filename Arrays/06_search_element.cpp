11bc070085e3a3f2a0f19471a8bbdebde7fe6fee
SatyaSingh29 <satyasingh.bk@gmail.com>
2026-06-10 22:43:53 +0530

    Added element searching program

---
+++ Arrays/06_search_element.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,target;
+    int index=-1;
+    cout << "Enter size of array:" << " ";
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    
+    cout << "Enter element to search:" <<" ";
+    cin >> target;
+    
+    for(int i=0;i<n;i++){
+        if(arr[i]==target){
+            index=i;
+            break;
+        }
+    }
+    if(index==-1){
+        cout << "Not found";
+    }
+    else cout <<"Found at index" << " " <<index;
+
+return 0;
+}
