fc716b9cf07c1eb8db405ff9815fd0e25133da2f
SatyaSingh29 <satyasingh.bk@gmail.com>
2026-06-10 22:08:07 +0530

    Added smallest element program

---
+++ Arrays/smallest.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array:" << " ";
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    int smallest =arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i]<smallest)
+            smallest=arr[i];
+    }
+    
+    cout << "Smallest element is" <<" " <<smallest;
+    
+
+    return 0;
+}
