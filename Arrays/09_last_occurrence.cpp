ebafa6955aa930b5f1790895af08d4aa87686379
SatyaSingh29 <satyasingh.bk@gmail.com>
2026-06-10 22:57:53 +0530

    Added last occurrence of element program

---
+++ Arrays/09_last_occurrence.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,target;
+    int index=-1;
+    cout << "Enter size of array:" << " ";
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    
+    cout << "Enter element to search:" <<" ";
+    cin >> target;
+    
+    for(int i=0;i<n;i++){
+        if(arr[i]==target){
+            index=i;
+        }
+    }
+    if(index==-1){
+        cout << "Not found";
+    }
+    else cout <<"Last Occurrence at index" << " " <<index;
+
+return 0;
+}
