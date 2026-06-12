79ef3bc574e98cc9d02403624adb3f6763bca1dc
Satya Singh <satyasingh.bk@gmail.com>
2026-06-12 22:48:17 +0530

    factorial using function program added

---
+++ Functions/04_factorial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int factorial(int n){
+    int fact=1;
+    for(int i=2;i<=n;i++){
+        fact*=i;
+    }
+    return fact;
+}
+int main(){
+    int n;
+    cout << "Enter number" <<" ";
+    cin >> n;
+    cout << "Factorial" <<" " << factorial(n);
+return 0;
+}
