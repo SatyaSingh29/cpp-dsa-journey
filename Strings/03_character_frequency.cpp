f56f83e2233d16ffc534c336d135d13fb6c1344b
Satya Singh <satyasingh.bk@gmail.com>
2026-06-16 11:46:53 +0530

    character frequency counter program added

---
+++ Strings/03_character_frequency.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    getline(cin,s);
+    
+    for(int i=0;i<s.length();i++){
+        int flag=0;
+        for(int j=0;j<i;j++){
+            if(s[i]==s[j]){
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0){
+            int count=0;
+            for(int j=0;j<s.length();j++){
+                if(s[i]==s[j]){
+                    count++;
+                }
+            }
+            cout<<s[i]<<"-->"<<count<<endl;
+        }
+    }
+    return 0;
+}
